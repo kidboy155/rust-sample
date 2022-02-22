16d88815c9b0f52ac32094051145cd036e0f6d01
quocNV_vn <quoc.nguyen@drjoy.vn>
2022-02-22 18:24:05 +0700

    refactoring code

--- s02_gessing_game/src/main.rs
+++ s02_gessing_game/src/main.rs
@@ -1,17 +1,18 @@
-
+use rand::Rng;
 use std::io;
 use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = 1;
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Faild to read line.");
+        let mut guess_str = String::new();
+        io::stdin().read_line(&mut guess_str).expect("Faild to read line.");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u32 = match guess_str.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Input must be number.");
