b34b4edf0583b20b06535b185e13052f4d3f059e
quocNV_vn <quoc.nguyen@drjoy.vn>
2022-03-03 23:52:24 +0700

    add ownership

--- s03_overview_concept/src/main.rs
+++ s03_overview_concept/src/main.rs
@@ -1,7 +1,27 @@
+use std::io;
 fn main() {
     // variable_sample()
     // function_sample()
-    control_flow_sample()
+    // control_flow_sample()
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+     let mut index = String::new();
+     io::stdin()
+     .read_line(&mut index)
+     .expect("Faild to read line");
+
+     let index: usize = index.trim()
+     .parse()
+     .expect("Index entered was not a number");
+
+     let element = a[index];
+
+     println!(
+         "The value of the element at index {} is {}",
+         index, element
+     );
+
 }
 /*
  * Variable
