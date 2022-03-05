a5c737f2017319c6db744a42374f945be910a315
quocNV_vn <quoc.nguyen@drjoy.vn>
2022-03-05 14:42:16 +0700

    more ownership

--- s04_ownership/src/main.rs
+++ s04_ownership/src/main.rs
@@ -1,5 +1,36 @@
 
+//##########################################################################################
+// Borring &
+fn main() {
+    // let s1 = String::from("hello");
+    // let len = calculate_length(&s1);
+    // println!("The length of '{}' is {}.", s1, len);
 
+    // let mut s = String::from("hello");
+    // change(&mut s);
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+
+    // println!("{}, {}, and {}", r1, r2, r3);
+    println!("{} and {}", r1, r2);
+    // r1 and r2 are no longer used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
+
+// fn calculate_length(s: &String) -> usize { // s is a reference to a String
+//     s.len()
+// } // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, nothing happens.
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
 //##########################################################################################
 
 
@@ -29,16 +60,16 @@
 //     println!("{}", some_integer);
 // } // Here, some_integer goes out of scope. Nothing special happens.
 //##########################################################################################
-fn main() {
-    let s1 = gives_ownership();         // gives_ownership moves its return
-                                        // value into s1
+// fn main() {
+    // let s1 = gives_ownership();         // gives_ownership moves its return
+    //                                     // value into s1
 
-    let s2 = String::from("hello");     // s2 comes into scope
+    // let s2 = String::from("hello");     // s2 comes into scope
 
-    let s3 = takes_and_gives_back(s2);  // s2 is moved into
-                                        // takes_and_gives_back, which also
+    // let s3 = takes_and_gives_back(s2);  // s2 is moved into
+    //                                     // takes_and_gives_back, which also
                                         // moves its return value into s3
-} // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
+// } // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
   // moved, so nothing happens. s1 goes out of scope and is dropped.
 
 fn gives_ownership() -> String {             // gives_ownership will move its
@@ -59,19 +90,19 @@ fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
     a_string  // a_string is returned and moves out to the calling function
 }
 //##########################################################################################
-fn main1() {
-    let s1 = String::from("hello");
+// fn main1() {
+//     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+//     let (s2, len) = calculate_length(s1);
 
-    println!("The length of '{}' is {}.", s2, len);
-}
+//     println!("The length of '{}' is {}.", s2, len);
+// }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len(); // len() returns the length of a String
 
-    (s, length)
-}
+//     (s, length)
+// }
 //##########################################################################################
 //##########################################################################################
 //##########################################################################################
